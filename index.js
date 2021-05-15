4e2a57bc5b8c50d6c029449a83350d93649113bb
guitongli <guitong.lee.contact@gmail.com>
2021-05-15 15:55:57 +0200

    building hamburger

--- index.js
+++ index.js
@@ -14,3 +14,41 @@ gsap.to(sections, {
     end: () => "+=" + document.querySelector(".container").offsetWidth
   }
 });
+
+
+ 
+    // const hamburger = document.getElementsByClassName('.burger')[0];
+    // $hamburger= 
+	const nav = useRef();
+	const menuNav = useRef();
+	const navLink = useRef();
+	const navItem = useRef([]);
+	var showMenu = false;
+	const video = useRef();
+	const producer = useRef();
+
+	function toggleMenu() {
+		if (!showMenu) {
+			hamburger.classList.add('open');
+			nav.current.className = "nav open";
+			menuNav.current.className = "menu-nav open";
+			// 			for (var i =0; i<=4; i++){
+			// navLink.current[i].className = "menu-nav__link open";
+			// 			}
+			for (var j = 0; j <= 4; j++) {
+				navItem.current[j].className = "menu-nav__item active open";
+			}
+			// navItem.current.className="menu-nav__item active open"
+			showMenu = true;
+		} else {
+			hamburger.current.className = "menu-btn__burger";
+			nav.current.className = "nav";
+			menuNav.current.className = "menu-nav";
+			navLink.current.className = "menu-nav__link";
+			for (var i = 0; i <= 4; i++) {
+				navItem.current[i].className = "menu-nav__item active";
+			}
+
+			showMenu = false;
+		}
+	}
