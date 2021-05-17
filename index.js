a4620a954dc4a4ed5e632f25522bd3656a6890c1
guitongli <guitong.lee.contact@gmail.com>
2021-05-17 23:00:17 +0200

    fixing hidden menu

--- index.js
+++ index.js
@@ -17,35 +17,33 @@ gsap.to(sections, {
 
 
  
-    const hamburger = document.getElementsByClassName('.burger')[0];
-    // $hamburger= 
-	const nav = document.querySelector('.nav');
-	const menuNav= document.querySelector('.menu-nav');
+    // const hamburger = document.getElementsByClassName('.burger')[0];
+$('#burger').click(function(){
+	
+		toggleMenu();
+	});
 	 
-	const navItem = document.querySelector('.nav__item__items')
 	var showMenu = false; 
 
 	function toggleMenu() {
 		if (!showMenu) {
 			console.log('clicked');
-			hamburger.classList.add('open');
-			nav.current.className = "nav open";
-			menuNav.current.className = "menu-nav open";
+			$('#burger').addClass('open');
+			$('#nav').addClass('open');
+			// menuNav.current.className = "menu-nav open";
 			// 			for (var i =0; i<=4; i++){
 			// navLink.current[i].className = "menu-nav__link open";
 			// 			}
-			for (var j = 0; j <= 4; j++) {
-				navItem.current[j].className = "menu-nav__item active open";
-			}
+			// for (var j = 0; j <= 4; j++) {
+				$('.nav__items__item').addClass("open");
+			// }
 			// navItem.current.className="menu-nav__item active open"
 			showMenu = true;
 		} else {
-			hamburger.current.className = "menu-btn__burger";
-			nav.current.className = "nav";
-			menuNav.current.className = "menu-nav";
-			navLink.current.className = "menu-nav__link";
+			$('#burger').removeClass('open');
+			$('#nav').removeClass('open');
 			for (var i = 0; i <= 4; i++) {
-				navItem.current[i].className = "menu-nav__item active";
+			$('.nav__items__item').removeClass("open");
 			}
 
 			showMenu = false;
