a20ba9a8571ca1346e98cabc2cefc95b1b0315f8
guitongli <guitong.lee.contact@gmail.com>
2021-05-26 22:57:24 +0200

    blog page completed

--- index.js
+++ index.js
@@ -21,27 +21,6 @@ words.forEach(word=>{
 })
 
 
-// banner
-let duration = 10,
-		sections = gsap.utils.toArray(".panel"),
-		sectionIncrement = duration / (sections.length-1),
-		tl = gsap.timeline({
-			scrollTrigger: {
-				trigger: ".container",
-				pin: true,
-				scrub: 1,
-        snap: 1 / (sections.length - 1),
-				start: "top top",
-				end: "+=5000"
-			}
-		});
-
-tl.to(sections, {
-  xPercent: -100 * (sections.length -1),
-  duration: duration,
-  ease: "none"
-});
-
 
 
 // everything below this is just for the fading/scaling up which is NOT scrubbed - it's all dynamic, triggered when each section enters/leaves so that the fading/scaling occurs at a consistent rate no matter how fast you scroll!
@@ -227,3 +206,96 @@ window.addEventListener("scroll", function () { // or window.addEventListener("s
 //     autoRotate: 90,
 //   }
 // });
+
+
+// animated listing
+
+function animateFrom(elem, direction) {
+  direction = direction || 1;
+  var x = 0,
+    y = direction * 100;
+  if (elem.classList.contains("gs_reveal_fromLeft")) {
+    x = -100;
+    y = 0;
+  } else if (elem.classList.contains("gs_reveal_fromRight")) {
+    x = 100;
+    y = 0;
+  }
+  elem.style.transform = "translate(" + x + "px, " + y + "px)";
+  elem.style.opacity = "0";
+  gsap.fromTo(elem, { x: x, y: y, autoAlpha: 0 }, {
+    duration: 1.25,
+    x: 0,
+    y: 0,
+    autoAlpha: 1,
+    ease: "expo",
+    overwrite: "auto"
+  });
+} 
+$(window).on('load',()=>{
+
+var $grid = $('.grid').isotope({
+  itemSelector: ".grid-item",
+  layoutMode: 'fitRows',
+  //         layoutMode: 'masonry',
+  //         persentPosition:true,
+  // masonry: {
+  //   columnWidth: 50
+  // }
+});
+
+
+return $grid;
+
+});
+$(".button-group").on("click", "button", function () {
+
+  var filterValue = $(this).attr("data-filter");
+  console.log('clicked', filterValue);
+  $grid.isotope({ filter: filterValue });
+});
+
+
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () { // or window.addEventListener("scroll"....
+  var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+  if (st > lastScrollTop) {
+    console.log(st)
+    $('header').removeClass('sticky');
+   
+  } else if (st< lastScrollTop && st > 6500){
+     console.log('added')
+    $('header').addClass('sticky');
+
+
+
+  }  else if (st< lastScrollTop && st < 6500){
+     console.log('added')
+    $('header').removeClass('sticky');
+
+
+
+  } 
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
+var $grid = $('.grid').isotope({
+  itemSelector: ".grid-item",
+  layoutMode: 'fitRows',
+  //         layoutMode: 'masonry',
+  //         persentPosition:true,
+  // masonry: {
+  //   columnWidth: 50
+  // }
+});
+
+
+$(".button-group").on("click", "button", function () {
+
+  var filterValue = $(this).attr("data-filter");
+  console.log('clicked', filterValue);
+  $grid.isotope({ filter: filterValue });
+});
+
+ 
